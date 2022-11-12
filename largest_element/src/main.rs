b9b70ad5320108afb6555a8f029a3223113c93e2
Dillon Roller <dillonroller.18@gmail.com>
2022-11-12 15:27:39 -0500

    Largest value in slice

--- largest_element/src/main.rs
+++ largest_element/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let vec = vec![10, 20, 30];
+    let result = find_largest(&vec);
+
+    println!("{result}");
+}
+
+fn find_largest(vec: &[i32]) -> &i32 {
+    let mut largest = &vec[0];
+    for element in vec {
+        if element > largest {
+            largest = element;
+        }
+    }
+    largest
 }
