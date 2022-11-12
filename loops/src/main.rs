c1cd0e3b5f9bc3671b76665a7589276897ca1463
Dillon Roller <dillonroller.18@gmail.com>
2022-11-12 13:49:07 -0500

    Fix loops

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,18 +1,4 @@
 fn main() {
-    let mut counter = 0;
-
-    let result = loop {
-        counter += 1;
-
-        if counter == 10 {
-            break counter * 2;
-        }
-    };
-
-    println!("The result is {result}");
-}
-
-fn main2() {
     let mut count = 0;
     'counting_up: loop {
         println!("count = {count}");
